9db47a2b992cfb7ecc0122b29fdc5e248e43fef2
Lucas Glaydson <lucas.glaydson01@hotmail.com>
2026-03-18 07:20:20 -0300

    chore: Firts project configuration

---
+++ src/main/java/com/apointments/apointments/ApointmentsApplication.java
@@ -0,0 +1,13 @@
+package com.apointments.apointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApointmentsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ApointmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apointments/apointments/ApointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apointments.apointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
